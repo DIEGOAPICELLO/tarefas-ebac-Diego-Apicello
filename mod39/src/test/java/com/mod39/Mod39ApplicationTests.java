cb168e5927d09735afb46a83a4cd34535a9d2976
diegoapicello <diegoapicello@gmail.com>
2024-10-29 16:32:00 -0300

    mod40 - SpringBoot

---
+++ mod39/src/test/java/com/mod39/Mod39ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mod39;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mod39ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mod40/src/main/java/com/mod39/Mod39Application.java
@@ -0,0 +1,15 @@
+package com.mod39;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod39Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Mod39Application.class, args);
+
+	}
+
+}
