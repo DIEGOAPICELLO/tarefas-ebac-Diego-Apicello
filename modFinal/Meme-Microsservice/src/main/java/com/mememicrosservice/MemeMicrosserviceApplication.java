1be77c4f373339417928b0cf37732dd8e5873231
Diego Apicello <manoburn@gmail.com>
2025-07-06 08:14:56 -0300

    Criado projeto MemeMicrosservice

---
+++ modFinal/Meme-Microsservice/src/main/java/com/mememicrosservice/MemeMicrosserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mememicrosservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeMicrosserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeMicrosserviceApplication.class, args);
+	}
+
+}
